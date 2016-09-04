b327d8f7b324c94d91e9c937901fbe2c1705fb2c
Bucks0888 <adamtait97@gmail.com>
2016-09-04 01:15:29 -0400

    Dodge-Block v1.0
    Created on 9/4/16

---
+++ Assets/Scripts/HighScore.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour {
+
+    private Text highScoreText;
+    private string highScoreString;
+    private float highscore;
+
+    // Use this for initialization
+    void Start()
+    {
+        highScoreText = GameObject.Find("HighScoreNum").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        highscore = PlayerPrefs.GetFloat("HighScore");
+        highScoreString = highscore.ToString();
+        if (highscore < 10f)
+        {
+            highScoreString = highScoreString.Substring(0, 5) + "s";
+        }
+        else if (highscore >= 10f)
+        {
+            highScoreString = highScoreString.Substring(0, 6) + "s";
+        }
+        else if (highscore >= 100f)
+        {
+            highScoreString = highScoreString.Substring(0, 7) + "s";
+        }
+        highScoreText.text = highScoreString;
+	}
+}
